5435dfef9056b1f090bb647f1ce28f991257d2fd
Manuel de la Fuente <m@nueljl.in>
2022-06-04 13:41:35 +0200

    Big update, click for details
    Add hue, saturation and lightness shifting; Update README; Switch from tabs to 4 spaces; Clean up and comment the code a bit

--- README.md
+++ README.md
@@ -8,30 +8,69 @@ The code is probably ...not the best, but I guess it gets the job done.
 * [Node.js](https://nodejs.org/en/)
 * NPM (Node's package manager, included in Node)
 * [culori](https://github.com/Evercoder/culori) `npm install culori`
-* [chalk]() `npm install chalk`
+* [chalk](https://github.com/chalk/chalk) `npm install chalk`
 
 ## Arguments
+The info at the right should be read like the Kotlin range operator: (minimumValue..maximumValue) : fallbackValue. If the number you inputted is outside of the valid value ranges, it will too use the 'fallbackValue'
+
 * --h: Set the hue (0..360) : 0
 * --s: Set the saturation (0..1) : 1
+* --mil: Set the minimum lightness (0..1) : 0.05
+* --mal: Set the maximum lightness (0..1) : 0.95
 
-* --mil: Set the minimum lightness
-* --mal: Set the maximum lightness
+<br>
+
+* --hshift: Change the hue by n in each subsequent color (-20..20) : 0
+* --sshift: Change the saturation by n in each subsequent color (-1..1) : 0
+* --steps: Change the lightness by n in each subsequent color, from maxLightness until minLightness (0.01, 1) : 0.05
+    * Modifying this will result in smaller steps (more colors) or bigger steps (less colors) since it will reach minLightness earlier or later depending on it.
+
+<br>
 
 * -H: Hide text
     * --width: Set width of color swatch (2..12) : 4
+    * --height: Set height of color swatch (1..4) : 2
+
+<hr><br>
 
 A few example commands:
 
 1. `node palettegen.js --h=80 --s=0.75 -H --width=12 --height=2`<br>
+    Set the hue to 80, the saturation to 75%, hide the text and specify swatch width and height in characters (spaces)<br>
     ![Complex example with hidden text and custom saturation and size](./img/example1.png)
 
 2. `node palettegen.js --h=170`<br>
+    Set the hue to 170, and just use the default values (fallbackValue) for everything else<br>
     ![Basic example of just setting the hue](./img/example2.png)
 
-3. `node palettegen.js --h=70 --mil=0.75`
+3. `node palettegen.js --h=70 --mil=0.75`<br>
+    Set the hue to 70 and the minimum lightness to 75%
     ![Example of limited lightness range](./img/example3.png)
 
+4. `node palettegen.js --h=192 --hshift=4 -H --width=12 --height=2`<br>
+    Set the hue to 192, then, for every new color, [hue shift](https://www.blue-canary.net/miniature-painting/painting-tips-and-guides/hue-shifting/) 4 steps
+    ![Example of hue shifting](./img/example4.png)
 
-### Roadmap...?
-* Support custom amount of lightness steps (currently hardcoded to increments of 0.05)
-* Generate images with the palettes directly on em, somehow?
+5. `node palettegen.js --h=192 --s=0 --sshift=0.05 --hshift=2`<br>
+        Set the hue to 192, then, for every new color hue shift 2 steps, and increase saturation by 0.05
+    ![Example of both hue and saturation shifting](./img/example5.png)
+
+
+## Roadmap
+
+### Easy stuff
+* [x] ~~Support custom amount of lightness steps (currently hardcoded to increments of 0.05)~~
+* [ ] Single color syntax: `node palettegen --ok(192, 1, 0.5)`
+    * Currently possible but could be better: `node palettegen.js --h=40 --steps=0.5 --mal=0.5`<br>
+      ![Current syntax example](./img/example-single-color-currentsyntax.png)
+* [ ] Custom type of output: `node palettegen --h=236 --o=rgb | --o=hsl` etc
+
+### The harder stuff
+* [ ] Generate images with the palettes directly on them somehow
+* [ ] Check WCAG contrast and readjust the lightness on the fly to meet the required inputted contrast level (good luck with that future me)
+* [ ] Presets for h, s, l or any combination, like UI, digital painting etc
+* [ ] Option to generate complementary colors (specified in amount) <!-- not _that_ hard tho -->
+
+
+## License...?
+I was going to initially set it as MIT, seeing as both culori and chalk use it, but it's not really right if there's a giant section of code [copied from StackOverflow](https://stackoverflow.com/a/54098693/17629516)... lol. It's not like licenses matters that much for a small js file anyways, sooo
--- palettegen.js
+++ palettegen.js
@@ -1,10 +1,21 @@
+// Color lib
 import { rgb, formatHex, formatRgb, converter } from 'culori';
+
+// TUI color output lib
 import chalk from 'chalk';
 
 
-function getArgs() {  // this is all straight from https://stackoverflow.com/a/54098693/17629516
-    const args = {};  // i don't have this big of a brain. like, i understand what it does
-    process.argv      // but no way in hell i could come up with this myself
+// Getting the command line arguments :::::::::::::::::::::::::::::::::::::::::
+
+    /* 
+        This part is all from stackoverflow.com/a/54098693/17629516 (tysm!)
+        I don't have this big of a brain. Like, I understand what it does and
+        how it works but no way in hell I could come up with this myself.
+    */
+
+function getArgs() {  
+    const args = {};
+    process.argv
         .slice(2, process.argv.length)
         .forEach( arg => {
             // long arg
@@ -27,40 +38,75 @@ function getArgs() {  // this is all straight from https://stackoverflow.com/a/5
 const args = getArgs();
 
 
-
-function isValid(val, min, max)
+// Check if the arguments supplied are valid and within their range, and if they
+// aren't, fall back to the default ones
+function isWithinRange(val, min, max)
 {
-	if (val != null && val >= min && val <= max)
-		return true
+    if (val != null && val >= min && val <= max)
+        return true
 
-	else return false
+    return false
 }
 
 
+// Limit float decimals to two decimals
+function formatNum(number) {
+    let formattedNumber = Number(number).toFixed(2)
+    return formattedNumber
+}
 
-const minLightness = isValid(args.mil, 0, 1) ? args.mil : 0.05;
-const maxLightness = isValid(args.mal, 0, 1) ? args.mal : 0.95;
-const saturation   = isValid(args.s,   0, 1) ? args.s   : 1;
-const hue          = isValid(args.h, 0, 360) ? args.h   : 0;
+
+// Pass the arguments through isWithinRange, and set the value according to results
+const hue             = isWithinRange(args.h, 0, 360)       ? +args.h      : 0;
+const saturation      = isWithinRange(args.s,   0, 1)       ? +args.s      : 1;
+const minLightness    = isWithinRange(args.mil, 0, 1)       ? +args.mil    : 0.05;
+const maxLightness    = isWithinRange(args.mal, 0, 1)       ? +args.mal    : 0.95;
+
+const hueShift        = isWithinRange(args.hshift, -20, 20) ? +args.hshift : 0;
+const saturationShift = isWithinRange(args.sshift, -1, 1)   ? +args.sshift : 0;
+const steps           = isWithinRange(args.steps,  0.01, 1) ? +args.steps  : 0.05;
+
+const height          = isWithinRange(args.height, 1, 4)    ? args.height  : 2;
+const width           = isWithinRange(args.width,  2, 12)   ? args.width   : 4;
 
 
+// Create the variables to be iterated on each loop (created outside
+// due to variable scope)
+let currentHue;
+let currentSaturation;
+let currentLightness;
+
+
+console.log(''); // Newline before the palette
+
+// Iterate and generate the color palette
 for (
-	let currentLightness = maxLightness;
-	currentLightness >= minLightness;
-	currentLightness = Math.round((currentLightness - 0.05) * 100) / 100
+    currentHue = hue,
+    currentSaturation = saturation,
+    currentLightness = maxLightness;
+
+    currentLightness >= minLightness;
+
+    currentHue += hueShift,
+    isWithinRange(currentSaturation + saturationShift, 0, 1)
+        ? currentSaturation += saturationShift
+        : 1, 
+    currentLightness = Math.round((currentLightness - steps) * 100) / 100
 ) {
-	let color = {
-		mode: 'okhsl',
-		l: currentLightness,
-		s: saturation,
-		h: hue
-	}
-	let colorHex = formatHex(rgb(color))
-	let text = (color.l >= 0.7) ? '#000' : '#fff'
+    // Create Culori color obj
+    let color = {
+        mode: 'okhsl',
+        l: currentLightness,
+        s: currentSaturation,
+        h: currentHue
+    }
+
+    let colorHex = formatHex(rgb(color)) // Format to rgb, then hex
+
+    // If the lightness of the generated color is >=70%, set text to black
+    let text = (color.l >= 0.7) ? '#000' : '#fff'
 
     if (args.H) {
-        let height = isValid(args.height, 1, 4)  ? args.height : 2;
-        let width  = isValid(args.width,  2, 12) ? args.width  : 4;
         let output = "";
 
         for (var i = 0; i < width; i++) { output += " " }
@@ -69,14 +115,21 @@ for (
             console.log(chalk.bgHex(colorHex)(output));
         }
     } else {
+        let output = `  ${colorHex} --` +
+                     ` h${formatNum(color.h)}` + 
+                     ` s${formatNum(color.s)}` +
+                     ` l${formatNum(color.l)}  `
         console.log(
             chalk
                 .hex(text)
                 .bgHex(colorHex)
-                .bold(`  ${colorHex} -- h${color.h} s${Number(color.s).toFixed(2)} l${Number(color.l).toFixed(2)}  `)
+                .bold(output)
         );
     }
 }
 
+
 if (args.H)
-    console.log(`\nHue: ${hue} -- Saturation: ${saturation}\n`);
+    console.log(`\nHue: ${hue} -- Saturation: ${saturation}`);
+
+console.log('\n'); // Two newlines after the palette 
